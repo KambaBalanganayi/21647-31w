bf5a6598a2dd35f3538e0b51abc2b52a28c572e1
Kamba Balanganayi <79821137+KambaBalanganayi@users.noreply.github.com>
2022-10-22 03:34:09 -0400

    c9 ajout de css dans <style>

--- kb_carrousel.php
+++ kb_carrousel.php
@@ -12,7 +12,17 @@ Version: 1.0.0
 */
 
 function genere_boite(){
-    $contenu = "<div class='carrousel'>Carrousel</div>";
+    $contenu = "
+    <style>
+    .carrousel {
+        border:2px solid #aaa;
+        background-color: #0f0;
+        width: 200px;
+        height: 200px;
+    }
+    </style>
+    <div class='carrousel'>Carrousel</div>";
+
     return $contenu;
 }
 add_shortcode('kb_carrousel', 'genere_boite');
